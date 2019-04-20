fa11bf12d7561d7a1175e8b8bf4bb57ae1470e06
Alec <amejuev@gmail.com>
2019-04-21 00:12:55 +0300

    database connect test

---
+++ controllers/DbController.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 21.04.2019
+ * Time: 0:06
+ */
+
+namespace app\controllers;
+
+
+use yii\web\Controller;
+
+class DbController extends Controller
+{
+  public function actionIndex() {
+    $result = \Yii::$app->db->createCommand(
+      "SELECT * FROM frutella.test"
+    )->queryAll();
+
+    var_dump($result);
+    exit;
+  }
+}
