c3d254d4b2720c39911252de207329dc3e7bf18a
Alec <amejuev@gmail.com>
2019-04-21 03:08:34 +0300

    Авторизация
    настроена авторизация через базу данных

--- models/User.php
+++ models/User.php
@@ -10,22 +10,22 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
     public $authKey;
     public $accessToken;
 
-    private static $users = [
-        '100' => [
-            'id' => '100',
-            'username' => 'admin',
-            'password' => 'admin',
-            'authKey' => 'test100key',
-            'accessToken' => '100-token',
-        ],
-        '101' => [
-            'id' => '101',
-            'username' => 'demo',
-            'password' => 'demo',
-            'authKey' => 'test101key',
-            'accessToken' => '101-token',
-        ],
-    ];
+//    private static $users = [
+//        '100' => [
+//            'id' => '100',
+//            'username' => 'admin',
+//            'password' => 'admin',
+//            'authKey' => 'test100key',
+//            'accessToken' => '100-token',
+//        ],
+//        '101' => [
+//            'id' => '101',
+//            'username' => 'demo',
+//            'password' => 'demo',
+//            'authKey' => 'test101key',
+//            'accessToken' => '101-token',
+//        ],
+//    ];
 
 
     /**
@@ -33,7 +33,12 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findIdentity($id)
     {
-        return isset(self::$users[$id]) ? new static(self::$users[$id]) : null;
+        $response = \Yii::$app->db->createCommand("
+           SELECT * FROM frutella.users WHERE id = :id
+        ")->bindValue(':id', $id)
+          ->queryOne();
+
+        return isset($response) ? new static($response) : null;
     }
 
     /**
@@ -41,7 +46,11 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findIdentityByAccessToken($token, $type = null)
     {
-        foreach (self::$users as $user) {
+        $response = \Yii::$app->db->createCommand("
+             SELECT * FROM frutella.users
+        ")->queryAll();
+
+        foreach ($response as $user) {
             if ($user['accessToken'] === $token) {
                 return new static($user);
             }
@@ -58,7 +67,11 @@ class User extends \yii\base\BaseObject implements \yii\web\IdentityInterface
      */
     public static function findByUsername($username)
     {
-        foreach (self::$users as $user) {
+        $response = \Yii::$app->db->createCommand("
+           SELECT * FROM frutella.users
+        ")->queryAll();
+
+        foreach ($response as $user) {
             if (strcasecmp($user['username'], $username) === 0) {
                 return new static($user);
             }
